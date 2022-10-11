60677b94f70892b6d5fd5d469c8f3df5f6767266
thijs131 <thijs98joosten@gmail.com>
2022-10-11 11:48:19 +0200

    Add dynamic tracking and dual cameras

---
+++ Haunting memories/Assets/Scripts/NewBehaviour.cs
@@ -0,0 +1,17 @@
+// Make an empty game object and then just add this script to it
+// Make sure you have 2 cameras, each with a differnt target display
+// Not sure how well this translates to VR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < Display.displays.Length; i++)
+            Display.displays[i].Activate(1920,1080,60);
+    }
+
+}
