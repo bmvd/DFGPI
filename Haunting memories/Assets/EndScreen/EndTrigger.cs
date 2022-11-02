8245508e50857df39cb13ef528919c02f78d4381
thijs131 <thijs98joosten@gmail.com>
2022-11-02 21:55:32 +0100

    Updated ending, added items to house
    Ending now does not block teleport, added sound and light and ensured the "the end" message covers the whole screen. Also added some items to the house.

--- Haunting memories/Assets/EndScreen/EndTrigger.cs
+++ Haunting memories/Assets/EndScreen/EndTrigger.cs
@@ -8,19 +8,44 @@ public class EndTrigger : MonoBehaviour
     public GameObject Fade;
     public GameObject endscreen;
     public GameObject endscreen2;
+    public Light endLight;
+    public GameObject enemy;
+    public GameObject lights;
+    public AudioSource audioSource;
+    public AudioClip angelicChoir;
+    private float counter;
+    private bool endReached;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("player"))
         {
             Debug.Log("Win");
-            player.SetActive(false);
-            Fade.SetActive(true);
-            Invoke("EndScreen", 1.7f);
+
+            audioSource.PlayOneShot(angelicChoir, 1f);
+
+            float counter = 0f;
+            endReached = true;
+
+            Invoke("EndScreen", 10f);
         }
     }
+
+    private void Update()
+    {
+        if (endReached & counter < 500)
+        {
+            counter += 1;
+            endLight.intensity = counter / 35;
+        }
+    }
+
     private void EndScreen()
     {
+        player.SetActive(false);
+        enemy.SetActive(false);
+        lights.SetActive(false);
+        Fade.SetActive(true);
         endscreen.SetActive(true);
         endscreen2.SetActive(true);
 
